9392dd9957e101f00a155dcd9fbb6f114cd313fb
danthek <abraham8507@gmail.com>
2022-07-29 14:16:45 -0500

    replaced local port +ngrok tunel for cymatic hook

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 80;                  //Save the port number where your server will be listening
-
+/* const port = 80;                  //Save the port number where your server will be listening
+ */
 var bodyParser = require('body-parser')
 // create application/json parser
 var jsonParser = bodyParser.json()
@@ -12,35 +12,40 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
-app.get('/users', function(req, res){ // this is only to see how to send an html file as response
+app.get('/users',jsonParser,  function(req, res){ // this is only to see how to send an html file as response
   return res.sendFile('users.html',{root: __dirname});
 });
 
+// server hook to cymatic ( Important: On Cymatic app the /hook path must be separated)
+app.post('https://api.dev.cymatic.info/v2/playbook/hook', function(req, res){
+  //this is a callback function
+})
 
 
-/////////////////////////// WEBHOOK TESTING ////////////////////////////////
 
-app.post('/incident',jsonParser, function(req, res){
-  try {
-/*     console.log(req); // if we put req it shows  the whole post info */
-    console.log(req.body); // if we put req.body it shows only the data in the  body and not the whole post info
-    console.log(req.headers); // if we put req.headers it shows only the data in the  headers and not the whole post info
-    console.log(req.text); // if we put req.text it shows only the data in the  textand not the whole post info
-    return res.status(200).send('ok');
-  } catch (error) {
-    return res.status(400).send('Error')
-  }
- 
-});
-
-////////////////////////////////////////////////////////////////////////////////////
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-  console.log(`Now listening on port ${port}`); 
-});
-
+/////////////////////////////////////////// WEBHOOK TESTING with port 80 ang ngrok tunel/////////////////////////////////////////
 
 //Remember to use a tunnel to make the local host public in order to test the webhook , ngrok can be used
 //https://ngrok.com/
 //ngrok.exe
 //ngrok http 80
 //http://ipadress/inspect/http . use this to inspect the posts and gets, just change the ip address
+
+/* app.post('/incident',jsonParser, function(req, res){
+  try {
+    //console.log(req); // if we put req it shows  the whole post info 
+    console.log(req.body); // if we put req.body it shows only the data in the  body and not the whole post info
+    console.log(req.headers); // if we put req.headers it shows only the data in the  headers and not the whole post info
+    console.log(req.text); // if we put req.text it shows only the data in the  textand not the whole post info
+    return res.status(200).send('ok');
+  } catch (error) {
+    return res.status(400).send('Error')
+  } 
+ 
+}); 
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`); 
+});*/
+
+
